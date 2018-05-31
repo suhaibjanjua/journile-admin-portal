250ded1a299ba751cc7dd3718292db2ab9877f07
Suhaib Janjua <suhaib.janjua@gmail.com>
2018-05-31 21:31:53 +0500

    app routes are added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+    {
+      path: '',
+      component: LoginComponent
+    },
+    {
+      path: 'users',
+      component: UsersComponent
+    },
+    {
+      path: 'transactions',
+      component: TransactionsComponent
+    },
+    {
+      path: '**',
+      component: NotFoundComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: []
+})
+export class AppRoutingModule {
+}
