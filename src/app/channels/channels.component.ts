77448b29c2b28b6c1997cf14fd9db353d23d542b
Suhaib Janjua <suhaib.janjua@gmail.com>
2018-06-14 13:39:25 +0500

    crud functionality implemented for channels

--- src/app/channels/channels.component.ts
+++ src/app/channels/channels.component.ts
@@ -8,22 +8,82 @@ import { ChannelService } from '../services/channel.service';
 })
 export class ChannelsComponent implements OnInit {
   channels: any;
+  name: string;
+  channelUrl: string;
+  RequestError: boolean;
+  logoFile: File;
+  logoFileName: any;
+  journileAdminPostImage: any;
+  formData = new FormData();
 
   constructor( private channelService: ChannelService) {
   }
 
   ngOnInit() {
-
-     this.channelService.getAll().subscribe(response => {
-        console.log(response);
-
-        if (response.responseCode === 200) {
-          this.channels = response.details;
-        } else {
-          // this.invalidLogin = true;
-        }
-
-     });
+    this.getAll();
   }
 
+  getAll() {
+    this.channelService.getAll().subscribe(response => {
+      console.log(response);
+
+      if (response.responseCode === 200) {
+        this.channels = response.details;
+      } else {
+        // this.invalidLogin = true;
+      }
+
+   });
+  }
+
+  channelLogo($event) {
+    const inputValue = $event.target;
+     if ( null == inputValue || null == inputValue.files[0]) {
+        console.log('Input file error.');
+        return;
+     } else {
+        console.log(inputValue.files[0]);
+        console.log(inputValue.files[0].name);
+        this.logoFile = inputValue.files[0];
+        this.logoFileName = this.logoFile.name;
+        this.journileAdminPostImage = 'JournileAdminPostImage.' + this.logoFileName.split('.').pop();
+        this.formData.append('JournileAdminPostImage', this.logoFile, this.journileAdminPostImage);
+        console.log(this.formData);
+     }
+   }
+
+   update() {
+
+    this.channelService.create(this.formData, this.name, this.channelUrl, this.logoFile)
+    .subscribe(response => {
+      console.log(response);
+      if (response.responseCode === 200) {
+        console.log('Channel has been updated successfully.');
+        this.RequestError = false;
+      } else {
+        this.RequestError = true;
+      }
+    });
+
+  }
+
+  delete(id, name) {
+
+    if (confirm('Are you sure to delete "' + name + '" channel?')) {
+
+      this.channelService.delete(id)
+      .subscribe(response => {
+        console.log(response);
+        if (response.responseCode === 200) {
+          console.log('Channel is deleted successfully.');
+          alert('Channel is deleted successfully.');
+          this.getAll();
+        } else {
+          console.log(response.responseDescription);
+          alert(response.responseDescription);
+        }
+      });
+
+    }
+  }
 }
--- src/app/services/channel.service.ts
+++ src/app/services/channel.service.ts
@@ -4,29 +4,35 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ChannelService {
+  private url = 'http://api.journile.io/JRA/Ch';
   headers: any;
 
-  constructor(private http: Http) {
-    this.http = http;
-  }
+  constructor(private http: Http) {}
 
   getAll() {
-    return this.http.get('http://api.journile.io/JRA/Ch/GC').map(response  => response.json());
+    return this.http.get(this.url + '/GC').map(response  => response.json());
   }
 
-  create(resource) {
-    this.headers =  { AuthToken: '', Username: resource.username, Password: resource.password };
-    return this.http.post('http://api.journile.io/JRA/Ch/AddChannel', { headers: this.headers }).map(response  => response.json());
+  create(formData, name, channelUrl, logoFile) {
+    this.headers =  { Name: name, URL: channelUrl };
+
+    console.log(this.headers);
+
+    return this.http.post(this.url + '/AddChannel', formData, { headers: this.headers })
+    .map(res => res.json());
       // .catch(this.handleError);
   }
 
   update(resource) {
-    return this.http.patch('http://api.journile.io/JRA/Ch/UpdateChannel', { headers: this.headers }).map(response  => response.json());
+    return this.http.patch(this.url + '/UpdateChannel', { headers: this.headers }).map(response  => response.json());
       // .catch(this.handleError);
   }
 
   delete(id) {
-    return this.http.delete('http://api.journile.io/JRA/Ch/DeleteChannel', { headers: this.headers }).map(response  => response.json());
+    this.headers = { ID: id };
+
+    console.log(this.headers);
+    return this.http.delete(this.url + '/DeleteChannel', { headers: this.headers }).map(response  => response.json());
       // .catch(this.handleError);
   }
 
